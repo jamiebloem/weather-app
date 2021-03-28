31ef800edc209f4cf87988719a58afbf33eb71bc
Jamie Bloem <jamiebloem@hotmail.com>
2021-03-28 16:39:46 +0200

    added weatherdata and eventlisteners

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
+import axios from 'axios';
+
+const apiKey = '3931aecfc386dfe36534008feaacbea0';
 
 function App() {
-  return (
-    <>
-      <div className="weather-container">
+    const [weatherData, setWeatherdata] = useState(null);
+    const [location, setLocation] = useState('');
 
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar/>
+    async function fetchData() {
+        try {
+            const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+            setWeatherdata(result.data);
 
-          <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+            console.log(result.data)
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    return (
+        <>
+            <div className="weather-container">
 
-            <button type="button">
+                {/*HEADER -------------------- */}
+                <div className="weather-header">
+                    <SearchBar setLocationHandler={setLocation}/>
+
+                    <span className="location-details">
+      {weatherData &&
+      <>
+          <h2>{weatherData.weather[0].description}</h2>
+          <h3>{weatherData.name}</h3>
+          <h1>{weatherData.main.temp}</h1>
+      </>
+      }
+
+                        <button
+                            type="button"
+                            onClick={fetchData}
+                        >
               Haal data op!
             </button>
-          </span>
-        </div>
+    </span>
 
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu/>
 
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
-          </div>
-        </div>
 
-        <MetricSlider/>
-      </div>
-    </>
-  );
+                </div>
+
+                {/*CONTENT ------------------ */}
+                <div className="weather-content">
+                    <TabBarMenu/>
+
+                    <div className="tab-wrapper">
+                        Alle inhoud van de tabbladen komt hier!
+                    </div>
+                </div>
+
+                <MetricSlider/>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
 
-function SearchBar() {
-  return (
-    <span className="searchbar">
+function SearchBar( { setLocationHandler }) {
+    const [query, setQuery] = useState('');
+
+const handleClick = () => {
+    setLocationHandler(query);
+}
+
+const keyPressCheck = (e) => {
+    if (e.keyCode === 13) {
+        setLocationHandler(query);
+    }
+}
+
+    return (
+        <span className="searchbar">
       <input
-        type="text"
-        name="search"
-        placeholder="Zoek een stad in Nederland"
+          type="text"
+          name="search"
+          value={query}
+          onKeyDown={keyPressCheck}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Zoek een stad in Nederland"
       />
 
-      <button type="button">
+      <button type="button"
+              onClick={setLocationHandler}
+      >
         Zoek
       </button>
     </span>
-  );
+    );
 };
 
 export default SearchBar;
